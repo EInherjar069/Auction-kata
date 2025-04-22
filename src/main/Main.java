37e9f421253ab7d97c03ae6acff808ca0f3c60c1
EInherjar069 <hell.daoum@gmail.com>
2025-04-22 10:27:46 +0200

    feat: added json path for jar artifact usage

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,14 +3,24 @@ package main;
 import auction.*;
 import utils.JsonBidLoader;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
 
         Auction auction = new VickreyAuction();
 
-        // getting data from bids.json file in ressources and injecting it into the auction
-        JsonBidLoader.loadFromJson("bids.json", auction);
+
+        if (args.length > 0) {
+            // load from external file for jar artifact when project is built
+            Path inputPath = Paths.get(args[0]);
+            JsonBidLoader.loadFromFile(inputPath,auction);
+        } else {
+            // getting data from bids.json file in resources and injecting it into the auction
+            JsonBidLoader.loadFromJson("bids.json",auction);
+        }
 
         // starting an auction
         auction.startAuction();
--- src/utils/JsonBidLoader.java
+++ src/utils/JsonBidLoader.java
@@ -1,14 +1,38 @@
 package utils;
 
+import auction.VickreyAuction;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import auction.Auction;
 import auction.Bid;
 import participants.Bidder;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class JsonBidLoader {
+
+    public static void loadFromFile(Path path, Auction auction) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            AuctionData data = mapper.readValue(Files.newBufferedReader(path), AuctionData.class);
+
+            if (auction instanceof VickreyAuction vickreyAuction) {
+                vickreyAuction.setReservePrice(data.reservePrice);
+            }
+
+            for (AuctionData.BidderData bd : data.bidders) {
+                Bidder bidder = new Bidder(bd.name);
+                for (Double bidAmount : bd.bids) {
+                    auction.submitBid(new Bid(bidder, bidAmount));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void loadFromJson(String resourceName, Auction auction) {
         try {
